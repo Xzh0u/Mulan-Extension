c76bc5698498822273c05afde1c50b1a8f9de262
Xzh0u <littleland.sxz@gmail.com>
2020-06-03 01:20:21 +0800

    feat: add notes button

--- app/components/Notes.js
+++ app/components/Notes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStickyNote } from '@fortawesome/free-solid-svg-icons';
-import { IconButton } from '@material-ui/core';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { Button } from '@material-ui/core';
 
 const Notes = () => (
-  <IconButton>
-    <FontAwesomeIcon icon={faStickyNote} color="gray" size="sm" />
-  </IconButton>
+  <Button
+    variant="contained"
+    className="ml-justify-center ml-fixed ml-bottom-25 ml-right-25 ml-shadow-lg ml-rounded-full ml-h-16 ml-w-16 ml-z-9999 ml-bg-white">
+    <FontAwesomeIcon icon={faPencilAlt} color="#4a5568" size="2x" />
+  </Button>
 );
 
 export default Notes;
