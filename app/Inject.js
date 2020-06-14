c5330b8b23a51958888ccedb0bed0b9e370e7fe4
zhangqi <epicqi@163.com>
2020-06-14 12:08:47 +0800

    Jump to the corresponding time when clicking img

--- app/Inject.js
+++ app/Inject.js
@@ -22,6 +22,7 @@ const StyledDrawer = styled(Drawer)`
 const InjectApp = (props) => {
   const [isVisible, setVisible] = useState(true);
   const domRef = useRef(null);
+  // const [srcs, setSrcs] = useState(['https://i.loli.net/2020/06/01/7Zn5NDfe8iLWtaB.png', 'https://i.loli.net/2020/06/01/7Zn5NDfe8iLWtaB.png', 'https://i.loli.net/2020/06/01/7Zn5NDfe8iLWtaB.png', 'https://i.loli.net/2020/06/01/7Zn5NDfe8iLWtaB.png', 'https://i.loli.net/2020/06/01/7Zn5NDfe8iLWtaB.png']);
   const [srcs, setSrcs] = useState([]);
 
   function base64toBlob(base64, type) {
@@ -41,16 +42,18 @@ const InjectApp = (props) => {
     //get img url from back-end
     if (srcs.length === 0) { //only load once
       axios
-      .get('http://127.0.0.1:2333/login')
+      .get('http://127.0.0.1:2333/get_frames')
       .then((response) => {
-        const data64 = response.data.data; //base64 format
+        const img64 = response.data.imgs; //base64 format
+        const times = response.data.times;
         const imgSrcs = [];
-        for (let i = 0, len = data64.length; i < len; i++) {
-          const res = base64toBlob(data64[i], 'image/jpeg'); //blob format
+        for (let i = 0, len = img64.length; i < len; i++) {
+          const res = base64toBlob(img64[i], 'image/jpeg'); //blob format
           const imgSrc = window.URL.createObjectURL(res); //url
-          imgSrcs.push(imgSrc);
+          imgSrcs.push([imgSrc, times[i]]);
         }
         setSrcs(imgSrcs);
+        // setTimes(times);
       })
       .catch((error) => {
         alert(error);
@@ -88,7 +91,7 @@ const InjectApp = (props) => {
         <StyledDrawer
           containerstyle={`
             width: calc(100%  - 360px);
-            height: 240px;
+            height: calc(30%);
             top: auto;
             bottom: 0px;
             visibility: invisible;
--- app/components/ImageCard.js
+++ app/components/ImageCard.js
@@ -5,18 +5,28 @@ import styled from 'styled-components';
 
 const StyledCard = styled(Card)`
   &:last-of-type {
-    margin-right: 20px;
+    margin-right: 0px;
+    margin-left: 0px;
+    margin-top: 0px;
+    margin-bottom: 0px;
   }
 `;
 
-const ImageCard = (props) => (
+const cardOnClick = (time) => {
+  // alert(time);
+  document.querySelector('video').currentTime = time;
+  document.querySelector('video').play();
+};
+
+const ImageCard = props => (
   <StyledCard
     raised
-    className="ml-flex-shrink-0 ml-min-w-0 ml-w-240 ml-h-44 ml-mx-4 ml-my-8">
+    className="ml-flex-shrink-0 ml-bottom-0 ml-min-w-0 ml-w-240 ml-mx-4 ml-my-12">
     <CardMedia
       component="img"
       alt="ppt"
-      image={props.src}
+      image={props.src[0]}
+      onClick={() => cardOnClick(props.src[1])}
     />
   </StyledCard>
 );
